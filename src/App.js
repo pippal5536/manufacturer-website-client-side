f23f390c057431ea681c588849ef545da396d85b
pippal5536 <pippal5536@gmail.com>
2022-05-22 18:00:56 +0600

    react router working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Blog from './Pages/Blog';
+import Navbar from './Shared/Navbar';
 
 function App() {
   return (
    <div className='text-6xl text-blue-600'>
-     Hi
+     <Navbar></Navbar>
+     <Routes>
+       <Route path="/blog" element={<Blog/>}></Route>
+     </Routes>
     </div>
   );
 }

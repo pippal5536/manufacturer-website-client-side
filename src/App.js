20aa3729470a4ec625fad114d24da4c852799556
pippal5536 <pippal5536@gmail.com>
2022-05-23 09:03:00 +0600

    navbar done

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Blog from './Pages/Blog';
 import Navbar from './Shared/Navbar';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
+import Portfolio from './Pages/Portfolio/Portfolio';
 
 
 
@@ -26,6 +27,7 @@ function App() {
 
           <Route path="/" element={<Home darkMode={darkMode} setDarkMode={setDarkMode} ></Home>}></Route>
           <Route path="/blog" element={<Blog />}></Route>
+          <Route path="/portfolio" element={<Portfolio />}></Route>
           <Route path="/login" element={<Login />}></Route>
           
           
--- src/Shared/Navbar.js
+++ src/Shared/Navbar.js
@@ -1,26 +1,47 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { SunIcon, MoonIcon } from '@heroicons/react/solid';
+import { SunIcon, MoonIcon, MenuAlt4Icon } from '@heroicons/react/solid';
 
 
 const Navbar = ({ darkMode, setDarkMode }) => {
+    const menuItems = <>
+        <li><Link to="/" >Home</Link></li>
+        <li><Link to="/blog">Blog</Link></li>
+        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/portfolio">My Portfolio</Link></li>
+        <label className="swap swap-rotate">
+
+            {
+                darkMode ? <> <input type="checkbox" onClick={() => setDarkMode(!darkMode)} /><SunIcon className='swap-off h-5 w-5' ></SunIcon>
+                    <MoonIcon className='swap-on h-5 w-5'></MoonIcon></>
+                    : <><input type="checkbox" onClick={() => setDarkMode(!darkMode)} /><SunIcon className='swap-on h-5 w-5' ></SunIcon>
+                        <MoonIcon className='swap-off h-5 w-5'></MoonIcon></>
+            }
+        </label>
+
+    </>
 
     return (
-        <div>
-            <Link to="/" >Home</Link>
-            <Link to="/blog">Blog</Link>
+        <div className="navbar bg-base-100 dark:bg-gray-800 shadow">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex="0" className="btn btn-ghost lg:hidden">
+                       <MenuAlt4Icon className='h-5 w-5'></MenuAlt4Icon>
+                    </label>
+                    <ul tabIndex="0" className="menu menu-compact  dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 dark:bg-gray-800 dark:text-white">
+                        {menuItems}
+                    </ul>
 
-            <Link to="/login">Login</Link>
-            
-            <label className="swap swap-rotate">
+                </div>
+                <a className="btn btn-ghost normal-case text-xl">Tool Manufacturer</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal p-0">
+                    {menuItems}
+                </ul>
+            </div>
 
-                {
-                    darkMode ? <> <input type="checkbox" onClick={() => setDarkMode(!darkMode)} /><SunIcon className='swap-off h-5 w-5' ></SunIcon>
-                        <MoonIcon className='swap-on h-5 w-5'></MoonIcon></>
-                        : <><input type="checkbox" onClick={() => setDarkMode(!darkMode)} /><SunIcon className='swap-on h-5 w-5' ></SunIcon>
-                            <MoonIcon className='swap-off h-5 w-5'></MoonIcon></>
-                }
-            </label>
+
 
         </div>
     );

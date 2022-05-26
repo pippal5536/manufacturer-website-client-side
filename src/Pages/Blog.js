d4faa0c72f060eea9d0e9f23c55a21fe56baa6c9
pippal5536 <pippal5536@gmail.com>
2022-05-26 10:37:57 +0600

    portfolio route created

--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -5,28 +5,29 @@ const Blog = () => {
     return (
         <div>
             <div className='m-8'>
-                <h1>How will you improve the performance of a React Application?</h1>
+                <h1 className='text-blue-600'>How will you improve the performance of a React Application?</h1>
                 <p>Using production build is one of the ways to improve performance of react app. I will keep components states in local if necessary. Re-renders affects react performance so memorize react components. </p>
             </div>
             <div className='m-8'>
-                <h1>What are the different ways to manage a state in a React App?</h1>
+                <h1 className='text-blue-600'>What are the different ways to manage a state in a React App?</h1>
                 <p>To manage local state, we use useState, useReducer. To manage global global state, we use Context API. To manage server state, we use SWR library, and React Query . To manage URL state, we use custom hooks like react router. In conclusion, the different ways to manage a state are using useState , useReducer, etc ;using  context api; using third party library like SWR or REACT QUERY; using custom hooks.</p>
             </div>
-            <div className="m-8"><h1>How does prototypical inheritance work?</h1>
+            <div className="m-8">
+                <h1 className='text-blue-600'>How does prototypical inheritance work?</h1>
                 <p></p>
             </div>
             <div className="m-8">
-                <h1>Why you do not set the state directly in React. For example, if you have const [products, setProducts] = useState([]). Why you do not set products = [...] instead, you use the setProducts</h1>
+                <h1 className='text-blue-600'>Why you do not set the state directly in React. For example, if you have const [products, setProducts] = useState([]). Why you do not set products = [...] instead, you use the setProducts?</h1>
                 <p>Setting the state directly causes odd bugs. Also it hampers component optimization as setting a state directly increases re-renders.</p>
             </div>
             <div className="m-8">
-                <h1>You have an array of products. Each product has a name, price, description, etc. How will you implement a search to find products by name?</h1>
-                <p>I can use filter function to implement a search to find products by name. For example, if an array has products that has same name, using array.filter() function will return all the products that had same name. Example:
+                <h1 className='text-blue-600'>You have an array of products. Each product has a name, price, description, etc. How will you implement a search to find products by name?</h1>
+                <p>I can use filter function to implement a search to find products by name. For example, if an array has products that has same name, using array.filter() function will return all the products that had same name. 
                 
                 </p>
             </div>
             <div className="m-8">
-                <h1>What is a unit test? Why should write unit tests?</h1>
+                <h1 className='text-blue-600'>What is a unit test? Why should write unit tests?</h1>
                 <p>Unit testing is to validate each unit of a software code to see if it runs as expected. We write unit tests to fix bugs as early as possible.</p>
             </div>
         </div>
--- src/Pages/Portfolio/Portfolio.js
+++ src/Pages/Portfolio/Portfolio.js
@@ -2,8 +2,31 @@ import React from 'react';
 
 const Portfolio = () => {
     return (
-        <div>
-            Portfolio
+        <div className=' m-4'>
+            <h1 className='text-center text-4xl font-bold'>My Portfolio</h1>
+
+            <div className='m-4 p-4'>
+                <p >First Name: Oishik </p>
+                <p > Last Name: Biswas</p>
+                <p >NickName/ Known As: Pippal</p>
+                <p >Email Address: pippal5536@gmail.com</p>
+            </div>
+            <div className='m-4 p-4'>
+                <h3 className='text-center text-2xl'>Educational Background:  </h3>
+                <p>SSC:<br /> Passyear: 2018 from Chittagong Cantonment Public College .<br /> HSC: <br />Passyear: 2020/ 2021(For corona situation exam was prosponed) from  Chittagong Cantonment Public College. <br /> Bsc:<br /> Currently enrolling in the University of the west of england, bristol. Expected pass year: 2025 </p>
+            </div>
+            <div className='m-4 p-4'>
+                <h3 className='text-center text-2xl'>Skills:</h3>
+                <p>Intermidiate Html, Intermidiate Css, Beginner Javascript, Beginner React, Beginner Node.js  </p>
+            </div>
+            <div className='m-4 p-4'>
+                <h3 className='text-center text-2xl'>My three projects</h3>
+                <p>1. <span>https://strong-kitsune-ec2605.netlify.app/</span></p>
+                <p>2. <span>https://assignment-3-miamiconvention-pippal5536.netlify.app/</span></p>
+                <p>3. <span>https://pippal5536.github.io/Assignment-1/index.html</span></p>
+            </div>
+
+
         </div>
     );
 };
